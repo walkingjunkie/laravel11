cbdb77f19f9db5dced06ca9017afee5129c9b391
walkingjunkie <112627569+walkingjunkie@users.noreply.github.com>
2022-11-14 10:14:36 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 
 Route::get('/', [AuthorController::class, 'index']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+// 追記：ここから
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
